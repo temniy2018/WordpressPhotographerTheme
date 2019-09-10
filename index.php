c61fe37f64547ade0305f17ae42cf02793515aa6
temniy2018 <r_zaitsev@bk.ru>
2019-09-10 23:06:10 +0300

    new sections and responsive styles

--- index.php
+++ index.php
@@ -6,7 +6,41 @@
 			<p>I have been providing photografer's service since 2005. For all of this years I accumulated a lot of experience. So I can find approach to any client</p>
 		</div>
 	</div>
+	<hr />
 	<div class="studioSection">
 		<h2>I use only modern and quality installation and buy it by proven brand</h2>
+	</div>
+	<div class="studioSection-mobile">
+		<img src="<?php echo get_theme_file_uri('/img/studio.jpg') ?>" class="studioSection-mobile_img">
+		<h2>I use only modern and quality installation and buy it by proven brand</h2>
+	</div>
+	<div class="euneed">
+		<h2>Everything you need</h2>
+		<p>My studio offers a variety of different sessions that include digital portraits and custom artwork. You can share your digital portraits with your friends, or you can archive them forever. Of course your portraits are meant to be displayed, and we offer a variety of beautiful options to complement your home decor.</p>
+	</div>
+	<div class="serviceSection">
+		<h2>Services</h2>
+		<div class="serviceSection_container">
+			<div class="serviceSection_service">
+				<img src="<?php echo get_theme_file_uri('img/wedding.jpg') ?>" class="serviceSection_service_img">
+				<h3>Wedding</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+			<div class="serviceSection_service">
+				<img src="<?php echo get_theme_file_uri('img/party.jpg') ?>" class="serviceSection_service_img">
+				<h3>Party</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+			<div class="serviceSection_service">
+				<img src="<?php echo get_theme_file_uri('img/personal.jpg') ?>" class="serviceSection_service_img">
+				<h3>Personal</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+			<div class="serviceSection_service">
+				<img src="<?php echo get_theme_file_uri('img/other.jpg') ?>" class="serviceSection_service_img">
+				<h3>Other</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+		</div>
 	</div>
 <?php get_footer(); ?>
