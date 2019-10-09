9f28cc7db7e60c64cab121d4f5fc620dc4a1e0a2
temniy2018 <r_zaitsev@bk.ru>
2019-10-09 22:13:51 +0300

    Some improvements and meta tags

--- header.php
+++ header.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 <head>
+	<meta charset="<?php bloginfo("charset") ?>"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<?php wp_head(); ?>
+	<title><?php wp_title('«', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 </head>
-<body>
+<body <?php body_class(); ?>>
 
+	<!-- your header logo is the title of the pages, so you need to make it as 'PhotoZone' title and 'PhotoZone portfolio' title -->
 	<header>
-		<p class="header_logo">PhotoZone</p>
-		<a class="header_portfolioLink" href="">Portfolio</a>
+		<p class="header_logo"><?php the_title(); ?></p>
+		<a class="header_portfolioLink" href="<?php echo site_url('/portfolio'); ?>">Portfolio</a>
 	</header>
+	
